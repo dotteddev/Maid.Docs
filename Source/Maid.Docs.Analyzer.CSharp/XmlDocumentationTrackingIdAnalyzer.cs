286c92a924edf479b50ce2762884bbc88bf5607b
Klára Vlkanová <emailodklary@gmail.com>
2024-09-05 20:44:26 +0200

    added analyzer for adding <trackingId>

---
+++ Source/Maid.Docs.Analyzer.CSharp/XmlDocumentationTrackingIdAnalyzer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Maid.Docs.Analyzer.CSharp
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class XmlDocumentationTrackingIdAnalyzer : DiagnosticAnalyzer
+	{
+		private const string MISSING_TRACKING_ID_DIAGNOSTIC_ID = "MD001";
+		public static readonly DiagnosticDescriptor MissingTrackingId = new DiagnosticDescriptor(MISSING_TRACKING_ID_DIAGNOSTIC_ID, "Missing trackingId", "Missing documentation <trackingId></trackingId>", "Docs", DiagnosticSeverity.Error, true, customTags: new[] { WellKnownDiagnosticTags.Build });
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return [MissingTrackingId]; } }
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.EnableConcurrentExecution();
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+			context.RegisterSyntaxTreeAction(AnalyzeSyntaxTree);
+		}
+
+		private void AnalyzeSyntaxTree(SyntaxTreeAnalysisContext context)
+		{
+			var comments = context.Tree.GetRoot().DescendantTrivia().Where(t => t.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia) || t.IsKind(SyntaxKind.MultiLineDocumentationCommentTrivia));
+			foreach (var comment in comments)
+			{
+				if(context.IsGeneratedCode) continue;
+				if (comment.Token.LeadingTrivia.ToString().Contains("<trackingId>")) 
+					continue;
+				context.ReportDiagnostic(Diagnostic.Create(MissingTrackingId, comment.GetLocation()));				
+			}
+		}
+	}
+
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(XmlDocumentationTrackingIdCodeFix)),Shared]
+	public class XmlDocumentationTrackingIdCodeFix : CodeFixProvider
+	{
+		public override ImmutableArray<string> FixableDiagnosticIds => ["MD001"];
+		public override Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			var diagnostic = context.Diagnostics.First();
+			context.RegisterCodeFix(CodeAction.Create("Add <trackingId>{{GUID}}</trackingId>", (c, ct) =>
+			{
+				return Fix(context.Document, diagnostic.Location, ct);
+			},"add-tracking-id-tag-to-doc-xml"), diagnostic);
+			return Task.CompletedTask;
+		}
+		private async Task<Document> Fix(Document document, Location location, CancellationToken cancellationToken)
+		{
+			var root = await document.GetSyntaxRootAsync(cancellationToken);
+			var node = root.FindNode(location.SourceSpan);
+			var comment = node.GetLeadingTrivia().First(t => t.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia) || t.IsKind(SyntaxKind.MultiLineDocumentationCommentTrivia));
+
+			
+
+			var newComment = comment.Token.LeadingTrivia.Add(SyntaxFactory.DocumentationCommentExterior($"/// <trackingId>{Guid.NewGuid()}</trackingId>\r\n"));
+			var newRoot = root.ReplaceToken(comment.Token, comment.Token.WithLeadingTrivia(newComment));
+			return document.WithSyntaxRoot(newRoot);
+		}
+		public override FixAllProvider GetFixAllProvider()
+		{
+			//FixAllProvider.Create(async (context, document, diagnostic) =>
+			//{
+			//	var cts = new CancellationTokenSource();
+			//	foreach (var diag in diagnostic)
+			//	{
+			//		if (FixableDiagnosticIds.Contains(diag.Id))
+			//			document = await AddTrackingIdAsync(document, diag.Location, cts.Token);
+			//	}
+			//	return document;
+			//});
+
+			//return base.GetFixAllProvider();
+			return WellKnownFixAllProviders.BatchFixer;
+		}
+	}
+}
--- Source/TestAssembly/MyClass.cs
+++ Source/TestAssembly/MyClass.cs
@@ -3,39 +3,54 @@
 /// <summary>
 /// My test class
 /// </summary>
+/// <trackingId>44de27be-135a-4712-b36d-f7dbcdc6290a</trackingId>
 public class MyClass : MyBaseClass, IMyInterface
 {
 	/// <summary>
 	/// my test field
 	/// </summary>
+	/// <trackingId>b25c3fc8-65e2-4008-8ca8-d17fae7f0355</trackingId>
 	readonly string _myField;
 
 	/// <summary>
 	/// constructor
-	/// </summary>
-	/// <param name="myField">my field</param>
+	/// </summary> 
+	/// <param name="myField">my field</param> 
+	/// <trackingId>5d482a41-846c-4a28-b2bc-b31989573c7c</trackingId>
 	public MyClass(string myField)
 	{
-		_myField = myField;
-	}
 
-	public string MyField { get; set; } = string.Empty;
+		_myField = myField;
+		Console.WriteLine();
+	}
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <trackingId>2bbb48c1-bc60-4606-b5da-82251dcefb7e</trackingId>
+public string MyField { get; set; } = string.Empty;
 
 	/// <summary>
 	/// my method
 	/// </summary>
 	/// <returns></returns>
-	public string GetMyField()
+	/// <trackingId>38bd7f1a-3959-41df-8dde-7e5639043dc2</trackingId>
+public string GetMyField()
 	{
-		return _myField;
+		if (true) return string.Empty;
 	}
 }
-
+/// <summary>
+/// 
+/// </summary>
+/// <trackingId>a11521bd-2173-4b34-8945-e369124449e6</trackingId>
 public class MyBaseClass
 {
 
 }
-
+/// <summary>
+/// 
+/// </summary>
+/// <trackingId>91f1ded8-6ef2-497e-b339-a301857cf904</trackingId>
 public interface IMyInterface
 {
 }
